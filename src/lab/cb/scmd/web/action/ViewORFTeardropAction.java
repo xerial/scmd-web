d6db3cde3a928c5381089f3f3343ccc1e594d344
leo <leo@localhost>
2005-02-18 09:02:28 +0000

    param -> paramID

--- src/lab/cb/scmd/web/action/ViewORFTeardropAction.java
+++ src/lab/cb/scmd/web/action/ViewORFTeardropAction.java
@@ -159,7 +159,7 @@ public class ViewORFTeardropAction extends Action
         {
             int paramID = param.getId();
             //StringElement label = new StringElement(param.getShortName());
-            Link label = new Link("ORFDataSheet.do?param=" + paramID, param.getShortName());
+            Link label = new Link("ORFDataSheet.do?paramID=" + paramID, param.getShortName());
             //labelRow.add(new AttributeDecollation(label, "title", param.getDisplayname()));
             labelRow.add(new AttributeDecollation(label, "title", param.getDisplayname()));
 
